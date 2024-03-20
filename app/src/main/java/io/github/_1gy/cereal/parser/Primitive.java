d00be40bee54d4177192d46fe6aa50452c0645de
1gy <42576495+1gy@users.noreply.github.com>
2024-03-20 01:50:47 +0000

    数値のパーサーを実装

--- app/src/main/java/io/github/_1gy/cereal/parser/Primitive.java
+++ app/src/main/java/io/github/_1gy/cereal/parser/Primitive.java
@@ -2,6 +2,52 @@ package io.github._1gy.cereal.parser;
 
 public final class Primitive {
 
+    public static final Parser<ByteSlice, Byte, String> beI8() {
+        return input -> {
+            if (input.length() < 1) {
+                return ParseResult.err("input too short");
+            }
+            var value = input.get(0);
+            var rest = input.slice(Range.from(1));
+            return ParseResult.ok(rest, value);
+        };
+    }
+
+    public static final Parser<ByteSlice, Short, String> beI16() {
+        return input -> {
+            if (input.length() < 2) {
+                return ParseResult.err("input too short");
+            }
+            var value = (short) ((input.get(0) << 8) | input.get(1));
+            var rest = input.slice(Range.from(2));
+            return ParseResult.ok(rest, value);
+        };
+    }
+
+    public static final Parser<ByteSlice, Integer, String> beI32() {
+        return input -> {
+            if (input.length() < 4) {
+                return ParseResult.err("input too short");
+            }
+            var value = (input.get(0) << 24) | (input.get(1) << 16) | (input.get(2) << 8) | input.get(3);
+            var rest = input.slice(Range.from(4));
+            return ParseResult.ok(rest, value);
+        };
+    }
+
+    public static final Parser<ByteSlice, Long, String> beI64() {
+        return input -> {
+            if (input.length() < 8) {
+                return ParseResult.err("input too short");
+            }
+            var value = ((long) input.get(0) << 56) | ((long) input.get(1) << 48) | ((long) input.get(2) << 40)
+                    | ((long) input.get(3) << 32) | ((long) input.get(4) << 24) | ((long) input.get(5) << 16)
+                    | ((long) input.get(6) << 8) | (long) input.get(7);
+            var rest = input.slice(Range.from(8));
+            return ParseResult.ok(rest, value);
+        };
+    }
+
     public static final Parser<ByteSlice, ByteSlice, String> tag(byte[] bytes) {
         return input -> {
             if (input.startsWith(bytes)) {
--- app/src/test/java/io/github/_1gy/cereal/parser/PrimitiveTest.java
+++ app/src/test/java/io/github/_1gy/cereal/parser/PrimitiveTest.java
@@ -9,6 +9,42 @@ import org.junit.jupiter.api.Test;
 public class PrimitiveTest {
 
     @Test
+    void beI8() {
+        var parser = Primitive.beI8();
+        var result = parser.parse(ByteSlice.of(new byte[] { 0x12, 0x34 }));
+        assertEquals(true, result.isOk());
+        assertArrayEquals(new byte[] { 0x34 }, result.rest().toByteArray());
+        assertEquals((byte) 0x12, result.value());
+    }
+
+    @Test
+    void beI16() {
+        var parser = Primitive.beI16();
+        var result = parser.parse(ByteSlice.of(new byte[] { 0x12, 0x34, 0x56 }));
+        assertEquals(true, result.isOk());
+        assertArrayEquals(new byte[] { 0x56 }, result.rest().toByteArray());
+        assertEquals((short) 0x1234, result.value());
+    }
+
+    @Test
+    void beI32() {
+        var parser = Primitive.beI32();
+        var result = parser.parse(ByteSlice.of(new byte[] { 0x12, 0x34, 0x56, 0x78, 0x01 }));
+        assertEquals(true, result.isOk());
+        assertArrayEquals(new byte[] { 0x01 }, result.rest().toByteArray());
+        assertEquals(0x12345678, result.value());
+    }
+
+    @Test
+    void beI64() {
+        var parser = Primitive.beI64();
+        var result = parser.parse(ByteSlice.of(new byte[] { 0x12, 0x34, 0x56, 0x78, 0x01, 0x23, 0x45, 0x67, 0x02 }));
+        assertEquals(true, result.isOk());
+        assertArrayEquals(new byte[] { 0x02 }, result.rest().toByteArray());
+        assertEquals(0x1234567801234567L, result.value());
+    }
+
+    @Test
     void test_tag() {
         var parser = tag("abc".getBytes());
         var result = parser.parse(ByteSlice.of("abcdef".getBytes()));
